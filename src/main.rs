221b962df6051a2f4fde22a5bb0c385ab8eb7b3c
Jepasq <jepasq111@gmail.com>
2022-10-14 11:54:28 +0200

    Add app::genesis implementation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use serde::{Serialize, Deserialize};
 
+use chrono::Utc;
+
+const GENHASH:
+    &str = "0000f816a87f806bb0073dcf026a64fb40c946b5abee2573702828694d5b4c43";
+
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Block {
     pub id: u64,
@@ -10,10 +15,31 @@ pub struct Block {
     pub nonce: u64,
 }
 
+#[derive(Debug)]
 pub struct App {
     pub blocks: Vec<Block>,
 }
 
+impl App {
+    fn new() -> Self {
+        Self { blocks: vec![] }
+    }
+
+    fn genesis(&mut self) {
+        let genesis_block = Block {
+            id: 0,
+            timestamp: Utc::now().timestamp(),
+            previous_hash: String::from("genesis"),
+            data: String::from("genesis!"),
+            nonce: 2836,
+            hash: GENHASH.to_string(),
+        };
+        self.blocks.push(genesis_block);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut a = App::new();
+    a.genesis();
+    println!("App is {:?}!", a);
 }
