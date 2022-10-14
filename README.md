28ee46f10b85f0d9b27f480f49a845a5f5b0ce45
Jepasq <jepasq111@gmail.com>
2022-10-14 11:45:11 +0200

    Add App and Block structs

---
+++ README.md
@@ -0,0 +1,20 @@
+# nodechain
+
+A blockchain-backed note taking application written in *rust*.
+
+# Building on Manjaro
+
+	sudo pacman -Sy rust
+	cargo build
+
+First build may be very long due to (300+) dependencies.
+
+# Rust mode in emacs
+
+At least in *emacs* 28 or if melpa is enabled :
+
+	<M-x> package-install <RET>
+	tust-mode <RET>
+	
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Block {
+    pub id: u64,
+    pub hash: String,
+    pub previous_hash: String,
+    pub timestamp: i64,
+    pub data: String,
+    pub nonce: u64,
+}
+
+pub struct App {
+    pub blocks: Vec<Block>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
