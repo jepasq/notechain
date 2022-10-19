9d9774de3d62cc613a2a518ea7b3612d3ce0c304
Jepasq <jepasq111@gmail.com>
2022-10-19 12:15:05 +0200

    Add basic p2p handling code

--- README.md
+++ README.md
@@ -9,12 +9,12 @@ A blockchain-backed note taking application written in *rust*.
 
 First build may be very long due to (300+) dependencies.
 
-# Rust mode in emacs
+# Rust major mode for emacs
 
 At least in *emacs* 28 or if melpa is enabled :
 
 	<M-x> package-install <RET>
-	tust-mode <RET>
+	rust-mode <RET>
 	
 
 
---
+++ src/p2p.rs
@@ -0,0 +1,56 @@
+pub static KEYS: Lazy = Lazy::new(identity::Keypair::generate_ed25519);
+pub static PEER_ID: Lazy = Lazy::new(|| PeerId::from(KEYS.public()));
+pub static CHAIN_TOPIC: Lazy = Lazy::new(|| Topic::new("chains"));
+pub static BLOCK_TOPIC: Lazy = Lazy::new(|| Topic::new("blocks"));
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ChainResponse {
+    pub blocks: Vec,
+    pub receiver: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct LocalChainRequest {
+    pub from_peer_id: String,
+}
+
+pub enum EventType {
+    LocalChainResponse(ChainResponse),
+    Input(String),
+    Init,
+}
+
+#[derive(NetworkBehaviour)]
+pub struct AppBehaviour {
+    pub floodsub: Floodsub,
+    pub mdns: Mdns,
+    #[behaviour(ignore)]
+    pub response_sender: mpsc::UnboundedSender,
+    #[behaviour(ignore)]
+    pub init_sender: mpsc::UnboundedSender,
+    #[behaviour(ignore)]
+    pub app: App,
+}
+
+impl AppBehaviour {
+    pub async fn new(
+        app: App,
+        response_sender: mpsc::UnboundedSender,
+        init_sender: mpsc::UnboundedSender,
+    ) -> Self {
+        let mut behaviour = Self {
+            app,
+            floodsub: Floodsub::new(*PEER_ID),
+            mdns: Mdns::new(Default::default())
+                .await
+                .expect("can create mdns"),
+            response_sender,
+            init_sender,
+        };
+        behaviour.floodsub.subscribe(CHAIN_TOPIC.clone());
+        behaviour.floodsub.subscribe(BLOCK_TOPIC.clone());
+
+        behaviour
+    }
+}
+
