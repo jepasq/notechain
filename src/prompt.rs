d391bd57f6f9fad3ad4e03b5970017fa860d2d7c
Jepasq <jepasq111@gmail.com>
2022-12-26 05:34:17 +0100

    More Prompt unit tests

--- src/prompt.rs
+++ src/prompt.rs
@@ -18,7 +18,24 @@ mod tests {
     use super::*;
     
     /// Can instanstiate prompt struct
+    #[test]
     fn test_inst() {
 	Prompt::new();
     }
+
+    /// Can access the text field
+    #[test]
+    fn test_text_access() {
+	let p = Prompt::new();
+	p.text;
+    }
+
+    /// Prompt text isn't empty
+    #[test]
+    fn test_text_not_empty() {
+	let p = Prompt::new();
+	assert_eq!(p.text.trim().is_empty(), false);
+    }
+
+    
 }
